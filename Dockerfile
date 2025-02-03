5eec32fa27ed48892e1fc116f3e50e6b0733f1db
Kevin Nitro <trannguyenthaibinh46@gmail.com>
2025-02-03 23:12:18 +0700

    init

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM dockurr/samba
+WORKDIR /app
+
+RUN apk --no-cache add fuse3
+COPY --from=rclone/rclone --chmod=755 /usr/local/bin/rclone /usr/local/bin/rclone
+COPY --chmod=755 start.sh /app/start.sh
+
+ENV TINI_SUBREAPER 1 # Need it for smb
+ENV NAME "rclone"
+ENV RW "false"
+
+ENTRYPOINT [ "/app/start.sh" ]
---
+++ start.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -e
+
+export RCLONE_CONFIG='/app/rclone/rclone.conf'
+PATH="$(pwd):$PATH"
+export PATH
+if ! rclone lsd AllDrives: >/dev/null; then
+  COMBINE_CONFIG=$(rclone backend -o config drives "${ROOT:-root:}")
+  echo "$COMBINE_CONFIG" >>"$RCLONE_CONFIG"
+fi
+
+rclone ${RCLONE_ARGS:+$RCLONE_ARGS} mount --cache-dir=./cache AllDrives: /storage --allow-non-empty --daemon --vfs-cache-mode=minimal ${RCLONE_MOUNT_ARGS:+$RCLONE_MOUNT_ARGS}
+# rclone mount --cache-dir=./cache root: /storage --allow-non-empty --daemon
+
+/sbin/tini -- /usr/bin/samba.sh
